29e3a6b65cc12fdc627db655e036786991cfafed
BattesZack <ST10483954@imconnect.edu.za>
2026-04-12 20:32:01 +0200

    Update Report.cs

--- Campus Management system/Report.cs
+++ Campus Management system/Report.cs
@@ -6,5 +6,25 @@ namespace Campus_Management_system
 {
     internal class Class1
     {
+      public static void GenerateStudentReport (Student student) 
+      {
+          Console.WriteLine("\n-------------Student Report-------------");
+
+          Console.WriteLine($"Name: {student.Name}");
+          Console.WriteLine($"Student Number: {student.StudentNumber}");
+          Console.WriteLine($"Course: {student.Course}");
+          Console.WriteLine($"Marks: {student.Marks}");
+
+          if (student.Marks > 50)
+          {
+              Console.WriteLine("Status: Passed")
+          }
+          else
+          {
+              Console.WriteLine("Status: Failed")
+          }
+
+          Console.WriteLine("--------------------------------------------\n")
+      }
     }
 }
