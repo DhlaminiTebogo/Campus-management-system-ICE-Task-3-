d08c9b6a38c853c14cc1e02af2e2d04a2561698c
BattesZack <ST10483954@imconnect.edu.za>
2026-04-13 10:38:17 +0200

    Update Registration.cs

--- Campus Management system/Registration.cs
+++ Campus Management system/Registration.cs
@@ -6,5 +6,49 @@ namespace Campus_Management_system
 {
     internal class Class1
     {
+    public List<Student> students = new List<Student>();
+
+    public void RegisterStudent()
+    {
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Email: ");
+        string email = Console.ReadLine();
+
+        Console.Write("Enter ID: ");
+        string id = Console.ReadLine();
+
+        Console.Write("Enter Student Number: ");
+        string studentNumber = Console.ReadLine();
+
+        Student student = new Student(name, email, id, studentNumber);
+
+        Console.Write("Enter Course: ");
+        student.Course = Console.ReadLine();
+
+        Console.Write("Enter Marks: ");
+        student.Marks = Convert.ToInt32(Console.ReadLine());
+
+        students.Add(student);
+
+        Console.WriteLine("Student registered successfully!\n");
+    }
+
+    public void ViewStudents()
+    {
+        foreach (var student in students)
+        {
+            student.DisplayInfo();
+            Console.WriteLine("----------------------");
+        }
+    }
+
+    public void GenerateReports()
+    {
+        foreach (var student in students)
+        {
+            Report.GenerateStudentReport(student);
+        }
     }
 }
